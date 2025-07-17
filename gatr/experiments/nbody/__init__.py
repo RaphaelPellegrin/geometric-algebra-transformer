0aa34469111d38b5b0883767c1e8df91acf8f864
Raphael <pellegrinraphael@Pellegrins-MacBook-Pro.local>
2025-07-17 00:31:33 -0400

    debug

--- gatr/experiments/nbody/__init__.py
+++ gatr/experiments/nbody/__init__.py
@@ -1,8 +1,22 @@
 # Copyright (c) 2023 Qualcomm Technologies, Inc.
 # All rights reserved.
+
+print("DEBUG: About to import NBodyDataset...")
 from .dataset import NBodyDataset
+
+print("DEBUG: NBodyDataset imported successfully")
+
+print("DEBUG: About to import NBodyExperiment...")
 from .experiment import NBodyExperiment
+
+print("DEBUG: NBodyExperiment imported successfully")
+
+print("DEBUG: About to import NBodySimulator...")
 from .simulator import NBodySimulator
+
+print("DEBUG: NBodySimulator imported successfully")
+
+print("DEBUG: About to import wrappers...")
 from .wrappers import (
     NBodyBaselineWrapper,
     NBodyGATrWrapper,
@@ -10,3 +24,5 @@ from .wrappers import (
     NBodySE3TransformerWrapper,
     NBodySEGNNWrapper,
 )
+
+print("DEBUG: All wrappers imported successfully")
